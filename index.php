90eab69a9d56243c2c978f017b0b194bc71446d9
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-06-09 17:26:29 -0500

    feat: :sparkles: Post finalizado
    Ya se guardan los datos en mockapi

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <?php
-    // $credenciales["http"]["method"] = "POST";
-    // $credenciales["http"]["header"] = "Content-type: application/json";
-    // $data = [
-    //     "cc"=>"123",
-    //     "nombre"=> "Miguel",
-    //     "apelldio"=> "Castro",
-    //     "edad"=> 23
-    // ];
-    // $data = json_encode($data);
-    // $credenciales["http"]["content"] = $data;
-    // $config = stream_context_create($credenciales);
-
-    // $_DATA = file_get_contents("https://6480e3fef061e6ec4d4a0194.mockapi.io/informacion", false, $config);
-    // print_r($_DATA);
+    function guardar(){
+        $credenciales["http"]["method"] = "POST";
+        $credenciales["http"]["header"] = "Content-type: application/json";
+        $data = [
+        "nombre"=> $_POST['nombre'],
+        "apellido"=> $_POST['apellido'],
+        "direccion"=> $_POST['direccion'],
+        "edad"=> $_POST['edad'],
+        "email"=> $_POST['email'],
+        "horario"=> $_POST['horaEntrada'],
+        "team"=> $_POST['team'],
+        "trainer"=> $_POST['trainer'],
+        "cc"=> $_POST['cedula']
+        ];
+        $data = json_encode($data);
+        $credenciales["http"]["content"] = $data;
+        $config = stream_context_create($credenciales);
+        $_DATA = file_get_contents("https://6480e390f061e6ec4d49febe.mockapi.io/informacion", false, $config);
+    };
+    if(isset($_POST['chulo'])){
+        guardar();
+    };
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -20,59 +28,62 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/stilo.css">
+    <link rel="stylesheet" href="css/estilo.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Registro</title>
 </head>
 <body>
     <div class="container">
-        <div class="caja1">
-            <div class="caja01">
-                <input type="text" name="nombre" placeholder="Nombre">
-                <input type="text" name="apellido" placeholder="Apellido">
-                <input type="text" name="direccion" placeholder="Direccion">
-            </div>
-            <div class="caja02">
-                <div class="logo">
-
+        <form method="POST">
+            <div class="caja1">
+                <div class="caja01">
+                        <input type="text" name="nombre" placeholder="Nombre">
+                        <input type="text" name="apellido" placeholder="Apellido">
+                        <input type="text" name="direccion" placeholder="Direccion">
                 </div>
-                <input type="text" name="edad" placeholder="Edad">
-                <input type="text" name="email" placeholder="Email">
+                <div class="caja02">
+                    <div class="logo">
+
+                    </div>
+                    <input type="text" name="edad" placeholder="Edad">
+                    <input type="text" name="email" placeholder="Email">
+                </div>
             </div>
-        </div>
-        <div class="caja2">
-            <div class="caja01">
-                <input type="text" name="horaEntrada" placeholder="Hora de entrada">
-                <input type="text" name="team" placeholder="Team">
-                <input type="text" name="trainer" placeholder="Trainer">
+            <div class="caja2">
+                <div class="caja01">
+                    <input type="text" name="horaEntrada" placeholder="Hora de entrada">
+                    <input type="text" name="team" placeholder="Team">
+                    <input type="text" name="trainer" placeholder="Trainer">
+                </div>
+                <div class="caja02">
+                    <table>
+                        <tr>
+                            <td><button type="submit" name="chulo" value="chulo"><i class="fa-solid fa-check"></i></button></td>
+                            <td><button type="submit" name="equis" value="equis"><i class="fa-solid fa-x"></i></button></td>
+                        </tr>
+                        <tr>
+                            <td><button type="submit" name="lapiz" value="lapiz"><i class="fa-sharp fa-solid fa-pen"></i></button></td>
+                            <td><button type="submit" name="lupa" value="buscar"><i class="fa-solid fa-magnifying-glass"></i></button><br></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2"><input type="text" name="cedula" placeholder="Cedula"></td>
+                        </tr>
+                    </table>
+                </div>
             </div>
-            <div class="caja02">
-                <table>
-                    <tr>
-                        <td><button type="submit" value="chulo"><i class="fa-solid fa-check"></i></button></td>
-                        <td><button type="submit" value="equis"><i class="fa-solid fa-x"></i></button></td>
-                    </tr>
-                    <tr>
-                        <td><button type="submit" value="lapiz"><i class="fa-sharp fa-solid fa-pen"></i></button></td>
-                        <td><button type="submit" value="buscar"><i class="fa-solid fa-magnifying-glass"></i></button><br></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"><input type="text" name="cedula" placeholder="Cedula"></td>
-                    </tr>
-                </table>
-            </div>
-        </div>
+        </form>
         <div class="caja3">
             <table>
                 <tr>
-                    <td><p>Nombre</p></td>
-                    <td><p>apellido</p></td>
-                    <td><p>direccion</p></td>
-                    <td><p>edad</p></td>
-                    <td><p>email</p></td>
-                    <td><p>horario de entrada</p></td>
-                    <td><p>team</p></td>
-                    <td><p>trainer</p></td>
+                    <td><h4><p>Nombre</p></h4></td>
+                    <td><h4><p>apellido</p></h4></td>
+                    <td><h4><p>direccion</p></h4></td>
+                    <td><h4><p>edad</p></h4></td>
+                    <td><h4><p>email</p></h4></td>
+                    <td><h4><p>horario de entrada</p></h4></td>
+                    <td><h4><p>team</p></h4></td>
+                    <td><h4><p>trainer</p></h4></td>
+                    <td><h4><p>flecha</p></h4></td>
                 </tr>
                 <tr>
                     <td><p>San</p></td>
@@ -83,6 +94,7 @@
                     <td><p>12:00pm</p></td>
                     <td><p>m3</p></td>
                     <td><p>Miguel</p></td>
+                    <td><button>▲</button></td>
                 </tr>
                 <tr>
                     <td><p>San</p></td>
@@ -93,6 +105,7 @@
                     <td><p>12:00pm</p></td>
                     <td><p>m3</p></td>
                     <td><p>Miguel</p></td>
+                    <td><button>▲</button></td>
                 </tr>
                 <tr>
                     <td><p>San</p></td>
@@ -103,6 +116,7 @@
                     <td><p>12:00pm</p></td>
                     <td><p>m3</p></td>
                     <td><p>Miguel</p></td>
+                    <td><button>▲</button></td>
                 </tr>
             </table>
         </div>
